f47041416a122b9b2e134c77963d78078261b282
Bryce Wilson <bryce@brycemw.ca>
2023-01-14 09:24:08 -0800

    Reset everything

---
+++ fit-finder-app/src/pages/CartPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function CartPage() {
+    return <div>
+
+    </div>
+}
--- fit-finder-app/src/pages/router.js
+++ fit-finder-app/src/pages/router.js
@@ -3,7 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 import { PageNotFound } from './404'
 import { About } from './About'
 import { Components } from './Components'
-import { Home } from './Home'
+import {Homepage} from "./Homepage";
+import {Upload} from "./Upload";
+import {LoadingPage} from "./LoadingPage";
+import {ItemPage} from "./ItemPage";
+import {CartPage} from "./CartPage";
 
 
 export const router = () => {
@@ -11,7 +15,12 @@ export const router = () => {
         <Switch>
             <Route path='/components' exact component={Components}/>
             <Route path='/about' exact  component={About}/>
-            <Route path='/' exact component={Home}/>
+            <Route path='/' exact component={Homepage}/>
+            <Route path='/HomePage' exact component={Homepage}/>
+            <Route path='/Upload' exact component={Upload}/>
+            <Route path='/LoadingPage' exact component={LoadingPage}/>
+            <Route path='/ItemPage' exact component={ItemPage}/>
+            <Route path='/CartPage' exact component={CartPage}/>
             <Route component={PageNotFound}/>
         </Switch>
     )
