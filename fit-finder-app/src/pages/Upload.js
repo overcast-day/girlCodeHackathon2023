ef2f41fd0de249ec957c0ff980e14d6fb60b1678
diana liang <aforestsmoon@gmail.com>
2023-01-14 11:24:30 -0800

    Upload Page Draft

--- fit-finder-app/src/pages/Upload.js
+++ fit-finder-app/src/pages/Upload.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, from "react";
+import Graphic from '../assets/svg/illustration-upload.svg';
+import {Link} from "react-router-dom";
+
+console.log(Graphic);
+window.location.href = "/LoadingPage"
 
 export function Upload() {
-    return <div>
+    return <div className="UploadPage">
+        <img src={Graphic} alt="Upload Page Illustration"/>
+        <p className="title">UPLOAD PHOTO HERE</p>
+        <p className="subtitle">
+            Upload an outfit from your phone to discover your recommendations
+        </p>
+        <button 
+            onclick="location.href='/LoadingPage';" 
+            value="Go To" />
 
     </div>
 }
